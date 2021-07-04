1b608a4f36a1748e1ba2a8604aa4463e7c729a6e
Hugo <hugo_matheuss@outlook.com>
2021-07-03 22:37:54 -0300

    configs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function __construct()
+    {
+        header('Access-Control-Allow-Origin: *');
+    }
+
     public function login(Request $request) 
     {
         $credentials = $request->only(['email', 'password']);
