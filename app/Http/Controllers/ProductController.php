ba75c9a0cb358bb5d5b7f3222738c8afc15da949
Hugo <hugo_matheuss@outlook.com>
2021-07-04 01:01:54 -0300

    configs

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,13 +12,6 @@ use App\Http\Resources\Product as ProductResource;
 
 class ProductController extends Controller
 {
-    public function __construct()
-    {
-        header('Access-Control-Allow-Origin:  *');
-        header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-        header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
-    }
-
     public function index()
     {
         return ProductResource::collection(Product::all());
